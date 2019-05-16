e8c9f2fe881e6b25f532d9195561ccf9daffb2df
Mateus Boiani <mateuz.boiani@gmail.com>
2019-05-16 13:31:08 -0300

    init commit

---
+++ source/constants.cu
@@ -0,0 +1,9 @@
+#include "constants.cuh"
+
+__constant__ float shift[100];
+__constant__ float m_rotation[10000];
+__constant__ Configuration params;
+__constant__ float F_Lower = 0.10;
+__constant__ float F_Upper = 0.90;
+__constant__ float T = 0.10;
+__constant__ char S_2DAB[150];
---
+++ source/constants.cuh
@@ -0,0 +1,34 @@
+#ifndef _CONSTANTS_H
+#define _CONSTANTS_H
+
+/*
+ * | ---------------------------
+ * | Data Type | size (byte)   |
+ * | ---------------------------
+ * | short     |      2        |
+ * | int       |      4        |
+ * | uint      |      4        |
+ * | float     |      4        |
+ * | double    |      8        |
+ * | ---------------------------
+ * | Total     | 40432 / 65536 |
+ * | ---------------------------
+ */
+
+typedef struct {
+    float x_min;
+    float x_max;
+    uint n_dim;
+    uint ps;
+    uint n_isl;
+} Configuration;
+
+extern __constant__ float shift[100];
+extern __constant__ float m_rotation[10000];
+extern __constant__ Configuration params;
+extern __constant__  float F_Lower;
+extern __constant__  float F_Upper;
+extern __constant__  float T;
+extern __constant__ char S_2DAB[150];
+
+#endif
